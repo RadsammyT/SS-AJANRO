81ce5382660b8759ab08de8a586b96b2a998cd62
RadsammyT <radsammyt@gmail.com>
2023-09-07 10:48:33 -0400

    emergency make batch script added in case shit dont work

---
+++ EmergencyMake.bat
@@ -0,0 +1,5 @@
+mkdir emergencyBuild
+mkdir emergencyBuild\Release
+mkdir emergencyBuild\Debug
+c++ src\main.cpp -o emergencyBuild\Release\ajanro.exe -O3 -DNDEBUG
+c++ src\main.cpp -o emergencyBuild\Debug\ajanro.exe -Og -DDEBUG
